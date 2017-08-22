747339d096471ab5b6f5725e3c65e10a1449bcaa
SJZeng <sjzeng@program.com.tw>
2017-08-23 01:02:49 +0800

    add test: remvoe user from team

--- tests/Feature/models/TeamTest.php
+++ tests/Feature/models/TeamTest.php
@@ -25,10 +25,8 @@ class TeamTest extends TestCase
     public function a_team_can_add_members()
     {
         $team = factory(Team::class)->create();
-
         $user = factory(User::class)->create();
         $userTwo = factory(User::class)->create();
-
         $team->add($user);
         $team->add($userTwo);
 
@@ -39,22 +37,54 @@ class TeamTest extends TestCase
     public function a_team_can_add_multiple_members_at_once()
     {
         $team = factory(Team::class)->create();
-
         $users = factory(User::class, 2)->create();
-
         $team->add($users);
 
         $this->assertEquals(2, $team->count());
     }
 
     /** @test */
+    public function a_team_can_remove_a_member()
+    {
+        $team = factory(Team::class)->create(['size' => 2]);
+        $users = factory(User::class, 2)->create();
+        $team->add($users);
+
+        $team->remove($users[0]);
+
+        $this->assertEquals(1, $team->count());
+    }
+
+    /** @test */
+    public function a_team_can_remove_more_than_one_member_at_once()
+    {
+        $team = factory(Team::class)->create(['size' => 3]);
+        $users = factory(User::class, 3)->create();
+        $team->add($users);
+
+        $team->remove($users->slice(0, 2));
+
+        $this->assertEquals(1, $team->count());
+    }
+
+    /** @test */
+    public function a_team_can_remove_all_members_at_once()
+    {
+        $team = factory(Team::class)->create(['size' => 2]);
+        $users = factory(User::class, 2)->create();
+        $team->add($users);
+
+        $team->restart();
+
+        $this->assertEquals(0, $team->count());
+    }
+
+    /** @test */
     public function a_team_has_a_maximum_size()
     {
         $team = factory(Team::class)->create(['size' => 2]);
-
         $user = factory(User::class)->create();
         $userTwo = factory(User::class)->create();
-
         $team->add($user);
         $team->add($userTwo);
 
