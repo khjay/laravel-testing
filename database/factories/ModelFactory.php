cbd10ac0de5f3a50b37aa0922ca182736f600f02
SJZeng <sjzeng@program.com.tw>
2017-08-23 00:38:18 +0800

    Testing Collaborators

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -28,3 +28,10 @@ $factory->define(App\Article::class, function (Faker\Generator $faker) {
         'title' => $faker->sentence,
     ];
 });
+
+$factory->define(App\Team::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'size' => 5
+    ];
+});
